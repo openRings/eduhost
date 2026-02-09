18216342a535f584dbecada1ebbd16749cfe0eb8
magwoo <magwood51@yandex.ru>
2026-02-09 22:46:58 +0300

    migrations: small `users` and `sessions` field updates

--- migrations/001_users_initial.sql
+++ migrations/001_users_initial.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
   id UUID PRIMARY KEY,
-  username TEXT NOT NULL,
+  username TEXT NOT NULL UNIQUE,
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
   patronymic TEXT,
--- migrations/004_sessions_initial.sql
+++ migrations/004_sessions_initial.sql
@@ -1,8 +1,8 @@
 CREATE TABLE sessions (
   id UUID PRIMARY KEY,
   user_id UUID NOT NULL REFERENCES users(id),
-  access_token_hash TEXT NOT NULL,
-  refresh_token_hash TEXT NOT NULL,
+  access_token TEXT NOT NULL,
+  refresh_token TEXT NOT NULL,
   access_duration INTERVAL NOT NULL,
   refresh_duration INTERVAL NOT NULL,
   created_at TIMESTAMPTZ NOT NULL
