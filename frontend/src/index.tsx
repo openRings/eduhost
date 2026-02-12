ae5025c032603f3b3b160c48dbd49bf11259381c
magwoo <magwood51@yandex.ru>
2026-02-12 21:12:35 +0300

    frontend: update uikit button, add disabled state

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -10,29 +10,39 @@ const App = () => {
     <div class="gap-xl mx-auto flex w-120 flex-col">
       <p class="py-20 text-center text-4xl text-green-700">Hello tailwind!</p>
       <div class="gap-md flex items-center">
-        <Button iconStart={<Settings />}>Нажми на меня</Button>
+        <Button iconStart={<Settings />} disabled>
+          Нажми на меня
+        </Button>
         <Button size="sm">Нажми на меня</Button>
       </div>
       <div class="gap-md flex items-center">
-        <Button variant="transparent">Нажми на меня</Button>
+        <Button variant="transparent" disabled>
+          Нажми на меня
+        </Button>
         <Button size="sm" variant="transparent">
           Нажми на меня
         </Button>
       </div>
       <div class="gap-md flex items-center">
-        <Button variant="accent">Нажми на меня</Button>
+        <Button variant="accent" disabled>
+          Нажми на меня
+        </Button>
         <Button size="sm" variant="accent">
           Нажми на меня
         </Button>
       </div>
       <div class="gap-md flex items-center">
-        <Button variant="primary">Нажми на меня</Button>
+        <Button variant="primary" disabled>
+          Нажми на меня
+        </Button>
         <Button size="sm" variant="primary">
           Нажми на меня
         </Button>
       </div>
       <div class="gap-md flex items-center">
-        <Button variant="danger">Нажми на меня</Button>
+        <Button variant="danger" disabled onclick={() => console.log(123)}>
+          Нажми на меня
+        </Button>
         <Button size="sm" variant="danger">
           Нажми на меня
         </Button>
--- frontend/src/shared/uikit/Button.tsx
+++ frontend/src/shared/uikit/Button.tsx
@@ -21,14 +21,15 @@ const baseClass =
 
 const variantClass: Record<Variant, string> = {
   default:
-    "ring-1 ring-inset ring-neutral-300 bg-gradient-to-t from-neutral-100 to-white hover:from-neutral-200 hover:to-neutral-100 hover:ring-neutral-400 active:from-neutral-300 active:to-neutral-200 active:ring-neutral-500",
-  transparent: "hover:bg-neutral-200 active:bg-neutral-300",
+    "ring-1 ring-inset ring-neutral-300 bg-gradient-to-t from-neutral-100 to-white hover:from-neutral-200 hover:to-neutral-100 hover:ring-neutral-400 active:from-neutral-300 active:to-neutral-200 active:ring-neutral-500 disabled:from-neutral-100 disabled:to-white disabled:ring-neutral-300 disabled:text-neutral-500",
+  transparent:
+    "hover:bg-neutral-200 active:bg-neutral-300 disabled:bg-transparent disabled:text-neutral-500",
   accent:
-    "ring-1 ring-inset ring-neutral-600 bg-neutral-700 text-white hover:bg-neutral-800 hover:ring-neutral-700 active:bg-neutral-900 active:ring-neutral-800",
+    "ring-1 ring-inset ring-neutral-600 bg-neutral-700 text-white hover:bg-neutral-800 hover:ring-neutral-700 active:bg-neutral-900 active:ring-neutral-800 disabled:bg-neutral-700 disabled:ring-neutral-600 disabled:text-white/60",
   primary:
-    "ring-1 ring-inset ring-primary-200 bg-primary-300 text-white hover:bg-primary-400 hover:ring-primary-300 active:bg-primary-500 active:ring-primary-400",
+    "ring-1 ring-inset ring-primary-200 bg-primary-300 text-white hover:bg-primary-400 hover:ring-primary-300 active:bg-primary-500 active:ring-primary-400 disabled:bg-primary-300 disabled:ring-primary-200 disabled:text-white/60",
   danger:
-    "ring-1 ring-inset ring-error-200 bg-error-300 text-white hover:bg-error-400 hover:ring-error-300 active:bg-error-500 active:ring-error-400",
+    "ring-1 ring-inset ring-error-200 bg-error-300 text-white hover:bg-error-400 hover:ring-error-300 active:bg-error-500 active:ring-error-400 disabled:ring-error-200 disabled:bg-error-300 disabled:text-white/60",
 };
 
 const sizeClass: Record<Size, string> = {
