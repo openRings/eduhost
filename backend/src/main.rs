bd360d90fa2ce085c06571e9d6b2527a1ba0b6e3
magwoo <magwood51@yandex.ru>
2026-02-25 00:06:34 +0100

    backend: add loki for tracing

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,12 +6,29 @@ use axum_cookie::CookieLayer;
 use eduhost::error::error_middleware;
 use eduhost::session::{Session, Student};
 use serde_json::json;
+use std::process;
 use tokio::net::TcpListener;
+use tracing_loki::url::Url;
+use tracing_subscriber::EnvFilter;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 
 mod auth;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let (layer, task) = tracing_loki::builder()
+        .extra_field("pid", format!("{}", process::id()))?
+        .build_url(Url::parse("http://loki:3100").unwrap())?;
+
+    tracing_subscriber::registry()
+        .with(EnvFilter::new("eduhost=info"))
+        .with(layer)
+        .init();
+
+    tracing::info!("backend starting..");
+    tokio::spawn(task);
+
     let pool = eduhost::database::init_database_pool()
         .await
         .context("failed to init database pool")?;
