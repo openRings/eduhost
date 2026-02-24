3637a5b03529436be3d40aeb32f9cc10a478e34a
magwoo <magwood51@yandex.ru>
2026-02-24 23:32:31 +0100

    frontend: update `Section` component, rename `title` field to `label`

--- frontend/src/shared/Section.tsx
+++ frontend/src/shared/Section.tsx
@@ -6,18 +6,18 @@ import { clsx } from "clsx";
 const baseClass = "p-6xl gap-3xl flex flex-col rounded-md bg-neutral-100";
 
 export type SectionProps = Omit<HTMLArkProps<"section">, "title"> & {
-  title?: JSX.Element;
+  label?: JSX.Element;
   description?: JSX.Element;
 };
 
 export function Section(props: SectionProps) {
-  const [_, attrs] = splitProps(props, ["title", "description", "class"]);
+  const [_, attrs] = splitProps(props, ["label", "description", "class"]);
 
   const classes = () => twMerge(clsx(baseClass, props.class));
 
   return (
     <section {...attrs} class={classes()}>
-      {props.title}
+      {props.label}
       {props.children}
     </section>
   );
