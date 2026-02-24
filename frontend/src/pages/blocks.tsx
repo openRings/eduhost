4d5265fd292717508c1f2396e410881e39aab1cb
magwoo <magwood51@yandex.ru>
2026-02-24 23:33:38 +0100

    frontend: update `/blocks` example page, use new features

--- frontend/src/pages/blocks.tsx
+++ frontend/src/pages/blocks.tsx
@@ -1,18 +1,32 @@
-import { BrushCleaning, ExternalLink, Plus } from "lucide-solid";
+import {
+  BrushCleaning,
+  ExternalLink,
+  PanelsTopLeft,
+  Plus,
+  Search,
+} from "lucide-solid";
 import { Block } from "../shared/Block";
 import { Section } from "../shared/Section";
 import { Button } from "../shared/uikit/Button";
 import { Input } from "../shared/uikit/Input";
 import { Select } from "../shared/uikit/Select";
+import { Label } from "../shared/uikit/Label";
 
 export default function () {
   return (
     <>
-      <Section title="Блоки">
+      <Section label="Блоки">
         <div class="gap-md flex">
           <Block
             class="h-36 grow"
-            title={[
+            label={<Label subLabel="Подзаголовок">Заголовок</Label>}
+            icon={<ExternalLink />}
+          >
+            123123
+          </Block>
+          <Block
+            class="h-36 grow"
+            label={[
               "Заголовок",
               <span class="text-neutral-500 underline">Подзаголовок</span>,
             ]}
@@ -22,17 +36,7 @@ export default function () {
           </Block>
           <Block
             class="h-36 grow"
-            title={[
-              "Заголовок",
-              <span class="text-neutral-500 underline">Подзаголовок</span>,
-            ]}
-            icon={<ExternalLink />}
-          >
-            123123
-          </Block>
-          <Block
-            class="h-36 grow"
-            title={[
+            label={[
               "Заголовок",
               <span class="text-neutral-500 underline">Подзаголовок</span>,
             ]}
@@ -42,20 +46,26 @@ export default function () {
           </Block>
         </div>
       </Section>
-      <Section class="gap-md flex-row">
-        <Input placeholder="Найти.." containerClass="grow" />
-        <Select
-          items={[
-            { label: "Проектирование и раз...", value: "1" },
-            { label: "Оптимизация веб прил...", value: "2" },
-          ]}
-          onselect={(value) => console.log(value)}
-          placeholder="Выберите предмет"
-        />
-        <Button iconStart={<BrushCleaning />}>Сбросить фильтры</Button>
-        <Button iconStart={<Plus />} variant="accent">
-          Новый проект
-        </Button>
+      <Section label={<Label icon={<PanelsTopLeft />}>Пример заголовка</Label>}>
+        <div class="gap-md flex">
+          <Input
+            icon={<Search />}
+            placeholder="Найти.."
+            containerClass="grow"
+          />
+          <Select
+            items={[
+              { label: "Проектирование и раз...", value: "1" },
+              { label: "Оптимизация веб прил...", value: "2" },
+            ]}
+            onselect={(value) => console.log(value)}
+            placeholder="Выберите предмет"
+          />
+          <Button iconStart={<BrushCleaning />}>Сбросить фильтры</Button>
+          <Button iconStart={<Plus />} variant="accent">
+            Новый проект
+          </Button>
+        </div>
       </Section>
     </>
   );
