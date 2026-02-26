609d4b7ae1c7cf0d5f8cffb508d29767512d589b
magwoo <magwood51@yandex.ru>
2026-02-26 14:33:48 +0100

    frontend: add util for work with auth

---
+++ frontend/src/utils/auth.ts
@@ -0,0 +1,49 @@
+import { createEffect, createSignal } from "solid-js";
+
+type RefreshSessionResponse = {
+  token: string;
+};
+
+const [accessToken, setAccessToken] = createSignal<string | undefined>(
+  undefined,
+);
+const [isAuthorized, setIsAuthorized] = createSignal(
+  !!localStorage.getItem("isAuthorized"),
+);
+
+export const currentAccessToken = () => accessToken();
+export const logout = () => setIsAuthorized(false);
+export { isAuthorized };
+
+createEffect(() => {
+  isAuthorized()
+    ? localStorage.setItem("isAuthorized", "true")
+    : localStorage.removeItem("isAuthorized");
+});
+
+let lock: undefined | Promise<any> = undefined;
+
+export async function refreshSession(authRedirect?: boolean) {
+  if (lock != undefined) {
+    await lock;
+    return;
+  }
+
+  let unlock: undefined | ((v: any) => void) = undefined;
+  lock = new Promise((resolve) => (unlock = resolve));
+
+  const response = await fetch("/api/auth/refresh", { method: "POST" });
+
+  if (!response.ok) {
+    setIsAuthorized(false);
+    authRedirect && window.location.assign("/signin");
+  }
+
+  const responseBody: RefreshSessionResponse = await response.json();
+
+  setAccessToken(responseBody.token);
+  setIsAuthorized(true);
+
+  unlock!(undefined);
+  lock = undefined;
+}
