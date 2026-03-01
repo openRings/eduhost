2e4fba0601afcec7265f76a21f7a355480f32520
magwoo <magwood51@yandex.ru>
2026-03-01 23:07:19 +0100

    frontend: add `clipboard` util

---
+++ frontend/src/utils/clipboard.ts
@@ -0,0 +1,26 @@
+import { info } from "./notifications";
+
+const unsecuredCopyToClipboard = (text: string) => {
+  const textArea = document.createElement("textarea");
+  textArea.style.position = "fixed";
+  textArea.value = text;
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+  try {
+    document.execCommand("copy");
+  } catch (err) {
+    console.error("Unable to copy to clipboard", err);
+  }
+  document.body.removeChild(textArea);
+};
+
+export function copyToClipboard(content: string, notificationMessage?: string) {
+  if (window.isSecureContext && navigator.clipboard) {
+    navigator.clipboard.writeText(content);
+  } else {
+    unsecuredCopyToClipboard(content);
+  }
+
+  info(notificationMessage ?? "Контент скопирован");
+}
